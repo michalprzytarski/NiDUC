7d2421f03cb6c9136972e1c5d7f52432819844ea
Matyjash <lengom4w@gmail.com>
2021-05-05 02:16:06 +0200

    Update 05.05.2021
    Zmiany głównie w dodawaniu pracowników ze względu na to, że teraz to oni szukają pracy i muszą mieć dostęp do dostaw i zamówień.

--- warehouse.py
+++ warehouse.py
@@ -22,37 +22,41 @@ class Warehouse:
         self.orders_sent = 0                                    # liczba wysłanych zamównień
         self.items_received = 0                                 # liczba towarów otrzymanych z dostawy
         self.break_time = self.envi.event()                     # wydarzenie przerwy TODO: przenieść do osobnego obiektu
+        self.tasks = simpy.Container(environ)
 
     #losowanie czasu oczekiwania
     def generate_wait_period(self):
         return numpy.random.exponential(15)                     # losowa z rozkładu wykładniczego
 
     #dodawanie pracowików
-    def hire_employees(self, num_of_employees):
+    def hire_employees(self, num_of_employees, orders, delivery):
         for i in range(0, num_of_employees):
-            emp = employee.Employee(self)                       # stworzenie obiektu pracownika
+            yield self.envi.timeout(0.1)
+            emp = employee.Employee(self,delivery, orders)      # stworzenie obiektu pracownika
             self.employees.put(emp)                             # dodatnie utworzonego pracownika do wszystkich pracowników
             print("Hired employee ", emp.employee_id)
+            self.envi.process(emp.run())
+
 
     #generowanie dostaw
     def generate_deliveries(self, delivery):
         while True:
-            period=self.generate_wait_period()                  # wylosowanej ilości czasu do odczekania
+            period = self.generate_wait_period()                 # wylosowanej ilości czasu do odczekania
             yield self.envi.timeout(period)                     # odczekanie wylosowanej ilości czasu
-            print("New delivery!")
-            self.envi.process(delivery.run())                   # wystartowanie procesu dostawy
+            print("Nowa dostawa!")
+            delivery.run()                                # wystartowanie procesu dostawy
 
     #generowanie zamówień
     def generate_orders(self, orders):
         while True:
-            period= self.generate_wait_period()                 # wylosowanie ilości czasu do odczekania
+            period = self.generate_wait_period()                 # wylosowanie ilości czasu do odczekania
             yield self.envi.timeout(period)                     # odczekanie wylosowanej ilości czasu
             print("Nowe zamówienia")
-            self.envi.process(orders.run())                     # wystartowanie procesu zamówień
+            orders.run()                     # wystartowanie procesu zamówień
     #generowanie przerw
     def generate_breaks(self, breakTime):
         while True:
-            yield  self.envi.timeout(BREAK_TIME)
+            yield self.envi.timeout(BREAK_TIME)
 
     #przerwa TODO: Przenieść do osobnego obiektu
     def breakTime(self):
@@ -64,14 +68,16 @@ class Warehouse:
 
 numpy.random.seed(0)
 env = simpy.rt.RealtimeEnvironment(SIMULATION_TEMPO)            # stworzenie środkiska symulacji
-war = Warehouse(100, env)                                         # stworzenie obiektu magazynu
-war.hire_employees(3)                                           # dodanie pracowników
+war = Warehouse(100, env)                                       # stworzenie obiektu magazynu
 delivery = delivery.Delivery(DELIVERY_TEMPO, war)               # stworzenie obiektu dostaw
 orders = orders.Orders(ORDERS_TEMPO, war)                       # stworzenie obiektu zamówień
+
 #deliveries=env.process(delivery.run())
 
 
 env.process(war.generate_deliveries(delivery))                  # rozpoczęcie procesu generowania dostaw
 env.process(war.generate_orders(orders))                        # rozpoczęcie procesu generowania zamówień
+env.process(war.hire_employees(3, orders, delivery))            # dodanie pracowników
+
 
 env.run(until=200)                                              # rozpoczęcie symulacji do zadanego czasu
