6154ba0500173d93dab439cee8062edb5412f562
Matyjash <lengom4w@gmail.com>
2021-05-07 02:53:25 +0200

    Klasa odpowiedzialna za przerwy

---
+++ break_time.py
@@ -0,0 +1,35 @@
+import simpy
+
+from employee import Employee
+
+
+class Break_time:
+
+     def __init__(self, break_times, break_duration, warehouse):
+         self.break_times = break_times
+         self.break_duration = break_duration
+         self.is_it_breaktime = False
+         self.last_break_time = 0
+         self.warehouse = warehouse
+         self.iterator = 0
+
+     def run(self):
+         while self.iterator < len(self.break_times):
+             yield self.warehouse.envi.timeout(self.break_times[self.iterator]-self.warehouse.envi.now)
+             self.is_it_breaktime = True
+             self.last_break_time = self.warehouse.envi.now
+             print("PRZERWA ! Pracownicy kończą swoje czynności i odpoczywają przez: ", self.break_duration)
+
+             for e in self.warehouse.employees:
+                 if e.current_action is not None and e.waiting is True:
+                    print(e.employee_id)
+                    e.interrupt()
+                    print ("Interrupted id: ", e.employee_id)
+
+
+             yield self.warehouse.envi.timeout(self.break_duration)
+             print("KONIEC PRZERWY! Pracownicy wracają do pracy")
+             self.is_it_breaktime = False
+             self.iterator += 1
+
+
