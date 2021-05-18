948a79e2917388340be6e3ad1b1a63bf6ce965e7
Matyjash <lengom4w@gmail.com>
2021-05-19 01:25:40 +0200

    Update 19.05

--- issues.py
+++ issues.py
@@ -1,17 +1,14 @@
-
 class Issues:
 
-     def __init__(self):
-         self.empty_war_count = 0
-         self.idle_war_count = 0
+    def __init__(self):
+        self.empty_war_count = 0
+        self.idle_war_count = 0
 
-     def warhouse_is_empty(self):
-         self.empty_war_count +=1
-         print("Magazyn jest pusty! To ", self.empty_war_count, " taka sytuacja")
+    def warhouse_is_empty(self):
+        self.empty_war_count += 1
+        print("Magazyn jest pusty! To ", self.empty_war_count, " taka sytuacja")
 
-     def warhouse_is_idle(self):
-         self.idle_war_count += 1
-         print("W magazynie brak jest jakichkolwiek zadan do wykonania! To ", self.idle_war_count, " taka sytuacja")
-
-
+    def warhouse_is_idle(self):
+        self.idle_war_count += 1
+        print("W magazynie brak jest jakichkolwiek zadan do wykonania! To ", self.idle_war_count, " taka sytuacja")
 
