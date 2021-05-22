61e500165f90bd40808c28a9bf30703e922a174f
Matyjash <lengom4w@gmail.com>
2021-05-22 21:36:07 +0200

    Zapamiętuje czasy problemów

--- issues.py
+++ issues.py
@@ -3,12 +3,16 @@ class Issues:
     def __init__(self):
         self.empty_war_count = 0
         self.idle_war_count = 0
+        self.empty_war_times = []
+        self.idle_war_times = []
 
-    def warhouse_is_empty(self):
+    def warhouse_is_empty(self, current_time):
         self.empty_war_count += 1
+        self.empty_war_times.append(current_time)
         print("Magazyn jest pusty! To ", self.empty_war_count, " taka sytuacja")
 
-    def warhouse_is_idle(self):
+    def warhouse_is_idle(self, current_time):
         self.idle_war_count += 1
+        self.idle_war_times.append(current_time)
         print("W magazynie brak jest jakichkolwiek zadan do wykonania! To ", self.idle_war_count, " taka sytuacja")
 
